a147974dc478c0d5bef42a77a414a66c8599f19e
David Pardo <davidpardomartin@gmail.com>
2023-05-10 23:58:54 +0200

    first commit

---
+++ app/Models/Task.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Task
+ *
+ * @property $id
+ * @property $name
+ * @property $description
+ * @property $start_date
+ * @property $end_date
+ * @property $priority
+ * @property $status
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Task extends Model
+{
+    
+    static $rules = [
+		'name' => 'required',
+		'description' => 'required',
+		'start_date' => 'required',
+		'end_date' => 'required',
+		'priority' => 'required',
+		'status' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name','description','start_date','end_date','priority','status'];
+
+
+
+}
---
+++ resources/views/task/form.blade.php
@@ -0,0 +1,39 @@
+<div class="box box-info padding-1">
+    <div class="box-body">
+        
+        <div class="form-group">
+            {{ Form::label('name') }}
+            {{ Form::text('name', $task->name, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Name']) }}
+            {!! $errors->first('name', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('description') }}
+            {{ Form::text('description', $task->description, ['class' => 'form-control' . ($errors->has('description') ? ' is-invalid' : ''), 'placeholder' => 'Description']) }}
+            {!! $errors->first('description', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('start_date') }}
+            {{ Form::text('start_date', $task->start_date, ['class' => 'form-control' . ($errors->has('start_date') ? ' is-invalid' : ''), 'placeholder' => 'Start Date']) }}
+            {!! $errors->first('start_date', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('end_date') }}
+            {{ Form::text('end_date', $task->end_date, ['class' => 'form-control' . ($errors->has('end_date') ? ' is-invalid' : ''), 'placeholder' => 'End Date']) }}
+            {!! $errors->first('end_date', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('priority') }}
+            {{ Form::text('priority', $task->priority, ['class' => 'form-control' . ($errors->has('priority') ? ' is-invalid' : ''), 'placeholder' => 'Priority']) }}
+            {!! $errors->first('priority', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('status') }}
+            {{ Form::text('status', $task->status, ['class' => 'form-control' . ($errors->has('status') ? ' is-invalid' : ''), 'placeholder' => 'Status']) }}
+            {!! $errors->first('status', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+
+    </div>
+    <div class="box-footer mt20">
+        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+    </div>
+</div>
