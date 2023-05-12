196a2c3e9efd023dd998e44144a4d49ba1fb4a63
David Pardo <davidpardomartin@gmail.com>
2023-05-12 04:49:37 +0200

    Refactor code for improved cleanliness

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::get('tasks',  'App\Http\Controllers\TaskController@fetchAll');
 Route::get('tasks/{id}', 'App\Http\Controllers\TaskController@fetchById');
